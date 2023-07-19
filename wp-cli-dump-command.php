e9f1c4c96986621adfc82f6c7d6f7fd7f7113fc2
Guillaume <guillaume.turpin45@gmail.com>
2023-07-19 16:31:52 +0200

    :fire: Clearing the plugin main file

--- wp-cli-dump-command.php
+++ wp-cli-dump-command.php
@@ -19,12 +19,14 @@
  * Author URI:        https://github.com/gturpin-dev
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Requires PHP:      8.1
+ * Requires at least: 6.0
  * Text Domain:       wp-cli-dump-command
  * Domain Path:       /languages
  */
 
+use WPCLI_DumpCommand\Plugin;
 use WPCLI_DumpCommand\Commands\WPCLI_Dump;
-use WPCLI_DumpCommand\Views\Dumps_List_Table;
 
 // Standard plugin security, keep this line in place.
 defined( 'ABSPATH' ) || die();
@@ -34,30 +36,4 @@ if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 }
 
-// Add option page with hello world inside
-add_action( 'admin_menu', function () {
-
-	// Create a custom option page in admin menu
-	add_menu_page(
-		__( 'Custom Dumps', 'wp-cli-dump-command' ),
-		__( 'Custom Dumps', 'wp-cli-dump-command' ),
-		'manage_options',
-		'wp-cli-dump-command',
-		function () {
-			echo '<div class="wrap">';
-			echo '<h1>' . esc_html( get_admin_page_title() ) . '</h1>';
-
-			$list_table = new Dumps_List_Table();
-			$list_table->prepare_items();
-			$list_table->display();
-			echo '</div>';
-		},
-		'dashicons-database-export',
-		85
-	);
-} );
-
-// Need WP-CLI to register the commands
-if ( ( defined( 'WP_CLI' ) && WP_CLI ) ) {
-	\WP_CLI::add_command( 'dump', WPCLI_Dump::class );
-}
+Plugin::init( __FILE__ );
