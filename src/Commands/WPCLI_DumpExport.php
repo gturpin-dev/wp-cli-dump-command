e29ce1aea4c0e687e1b7df2ca2aa113c7fcd7e54
Guillaume <guillaume.turpin45@gmail.com>
2023-07-26 17:27:10 +0200

    :sparkles: Adding languages dump command

--- src/Commands/WPCLI_DumpExport.php
+++ src/Commands/WPCLI_DumpExport.php
@@ -171,4 +171,38 @@ final class WPCLI_DumpExport extends WP_CLI_Command {
 
 		WP_CLI::success( sprintf( 'Uploads dumped successfully at "%s".', $export_file->get_dir_path() ) );
 	}
+
+	/**
+	 * Download a dump of the languages files
+	 * 
+	 * ## OPTIONS
+	 * 
+	 * [--name=<name>]
+	 * : The name of the file to save the dump. If not provided, a default name will be used.
+	 * 
+	 * ## EXAMPLES
+	 * wp dump uploads
+	 * wp dump uploads --name=custom_dump
+	 *
+	 * @param array $args The list of arguments
+	 * @param array $assoc_args The list of associative arguments
+	 *
+	 * @return void
+	 */
+	public function languages( array $args, array $assoc_args ): void {
+		// Assign default values
+		$assoc_args = wp_parse_args( $assoc_args, [
+			'name' => 'languages',
+		] );
+
+		// Create export file
+		try {
+			$export_file = new ExportFile( $assoc_args['name'] );
+			$export_file->create_from( WP_CONTENT_DIR . '/languages' );
+		} catch ( ExportFailedException $e ) {
+			WP_CLI::error( $e->getMessage() );
+		}
+
+		WP_CLI::success( sprintf( 'Languages dumped successfully at "%s".', $export_file->get_dir_path() ) );
+	}
 }
